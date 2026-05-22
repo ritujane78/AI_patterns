b70b8b62c69c3ad40c64e44b087028c99e6cba26
Ritu Bafna <ritujane78@gmail.com>
2026-05-22 09:53:32 +0545

    fix spellings, add screenshots

--- reflection_pattern_ollama.py
+++ reflection_pattern_ollama.py
@@ -18,7 +18,7 @@ task = """
 
 writer = AssistantAgent(
     name = "Writer",
-    system_message = "You are a writer. You write engging and concise articles"
+    system_message = "You are a writer. You write engagaing and concise articles"
     "(with title) on given topics. You must polish your writing based on the"
     "feedback you receive and give a refined version. Only return your final work"
     "without additional comment.",
